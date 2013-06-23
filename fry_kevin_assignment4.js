4ed5aaefd6c9dd401052e320544374ad5a421a4a
Kevin Fry <kevinfry@fullsail.edu>
2013-06-22 22:56:33 -0400

    added functions to test email and a url
    just need to edit the flowchart, but they are simple if statements just
    like the last one, but less complex

--- fry_kevin_assignment4.js
+++ fry_kevin_assignment4.js
@@ -3,6 +3,10 @@
 // Project 4
 // My Library of String, Number, and Array testing
 
+var myPhoneNumberTest = "215-888-2295";
+var myEmailTest = "kevinjfry@gmail.com";
+var myURLTest = "https://www.fryintl.com/"
+
 ////////////////////////////////////////////////////////////////////////////////
 // Does a string follow a 123-456-7890 pattern like a phone number?
 var testPhoneNumberPattern = function(phoneNumber){
@@ -33,10 +37,60 @@ var testPhoneNumberPattern = function(phoneNumber){
     }
 };
 
-var myPhoneNumberTest = "123-456-7890";
-boolPhoneNumber = testPhoneNumberPattern(myPhoneNumberTest);
-console.log("It's " + boolPhoneNumber + " that \"" + myPhoneNumberTest + "\" meets our phone number requirements.");
 
 ////////////////////////////////////////////////////////////////////////////////
 // Does a string follow an aaa@bbb.ccc pattern like an email address?
+var testEmailPattern = function(eMailAddress){
+    //
+    var thisEmailAddress = eMailAddress.split("@");  // seperate user from domain
+    var userEmail = thisEmailAddress[0];
+    var userDomain = thisEmailAddress[1].split(".");
+    var domainName = userDomain[0];
+    var domainLtd = userDomain[1];
+    
+    // construct email address from the variables
+    var constructEmail = userEmail + "@" + domainName + "." + domainLtd;
+    
+    // check email address reconstruction and compare it to the inputed email address
+    if (constructEmail === eMailAddress) {
+        // email addresses match, has @ and . in the right spots
+        // The full domain name may not exceed the length of 253
+        // http://en.wikipedia.org/wiki/Domain_Name_System
+        // 63 characters in the maximum length of a "label" (part of domain name separated by dot)
+        // .travel seems to be the longist ltd
+        // research comes from http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains
+        if ((domainName.length <= 63) && (domainLtd.length <= 6) &&
+            (domainName.length >= 2) &&  (domainLtd.length >= 2)  && (userEmail.length >= 1 )){
+            return true;
+        } else {
+            return false;
+        }
+        
+    } else {
+        // email addresses do not match
+        return false;
+    }
+};
 
+////////////////////////////////////////////////////////////////////////////////
+// Is the string a URL? (Does it start with http: or https:?)
+var testURL = function(url){
+    thisURL = url.split("//");
+    if ((thisURL[0] === "http:") || (thisURL[0] === "https:")) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
+
+
+boolPhoneNumber = testPhoneNumberPattern(myPhoneNumberTest);
+boolEmail = testEmailPattern(myEmailTest);
+boolURL = testURL(myURLTest);
+
+
+console.log("It's " + boolPhoneNumber + " that \"" + myPhoneNumberTest + "\" meets our phone number requirements.");
+console.log("It's " + boolEmail + " that \"" + myEmailTest + "\" meets our e-mail requirements.");
+console.log("It's " + boolURL + " that \"" + myURLTest + "\" meets our url requirements.");
