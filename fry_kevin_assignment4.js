96e563a1ae1824009895a36b44eaf3b1d42a3df6
Kevin Fry <kevinfry@fullsail.edu>
2013-06-27 03:18:28 -0400

    minor edit .0000000001
    lol

--- fry_kevin_assignment4.js
+++ fry_kevin_assignment4.js
@@ -229,6 +229,6 @@ console.log("We're changing \"" + myOldString + "\", from the old separator ("+m
 
 console.log("If we're charged " + myMoney + " for something, we're actually paying $" + numMoneyReformat + " for it.");
 
-console.log("We can convert a string, such as \""+myStr2Num+"\" to a number, " + strMyNumber+".");
+console.log("We can convert a string, such as \""+myStr2Num+"\" to a number, to " + strMyNumber+".");
 
 // end for now
